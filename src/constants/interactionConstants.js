a39ee8c87cd135656713228f8a17900ab7f50857
BOTREL Kilian <botrel.kilian1999@gmail.com>
2025-01-08 17:11:06 +0100

    feat(constants): add actions names and welcome message variables

---
+++ src/constants/interactionConstants.js
@@ -0,0 +1,9 @@
+export const Actions = {
+  WelcomeMessageModal: 'welcome-message-modal',
+  WelcomeMessageSlashCommand: 'welcome-message-setup',
+};
+
+export const WelcomeMessageVariables = {
+  RulesChannel: '{{rulesChannel}}',
+  User: '{{user}}',
+};
